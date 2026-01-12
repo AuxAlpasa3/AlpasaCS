f28c77f5310f47bf4d2897e9a49f3a8a24e3ff12
AuxiliarSistemas3 <auxiliarsistemas3@alpasa.com.mx>
2026-01-12 11:41:33 -0600

    Update BuscarBoletasBascula.php

--- Movil/BuscarBoletasBascula.php
+++ Movil/BuscarBoletasBascula.php
@@ -7,31 +7,33 @@ require_once '../api/db/conexion2.php';
 
 try {
         $sql = "SELECT t1.IdBoletas,
-        t1.Placas,
-        t1.Chofer,
-        t10.Cliente,
-        t7.TipoTransporte,
-        (SELECT STRING_AGG(Descripcion, ', ') WITHIN GROUP (ORDER BY Descripcion ASC)
-            FROM (SELECT DISTINCT t8.Descripcion
-                FROM t_boleta_det t2b
-                LEFT JOIN t_producto t8 ON t2b.Producto = t8.IdProducto
-                WHERE t2b.IdBoletasEnc = t1.Idboletas) AS productos) as Descripcion,
-        t9.Transportista,
-        t11.Ubicacion as Origen,
-        t12.Ubicacion as Destino,
-        (SELECT STRING_AGG(Placas, ', ') WITHIN GROUP (ORDER BY Placas ASC)
-            FROM (SELECT DISTINCT t2c.Placas
-                FROM t_boleta_det t2c
-                WHERE t2c.IdBoletasEnc = t1.Idboletas) AS placas) as PlacasRemolque
-    FROM t_boleta_enc as t1 
-    LEFT JOIN t_tipoTransporte as t7 ON t1.TipoTransporte = t7.IdTipoTransporte
-    LEFT JOIN t_transportista as t9 on t1.Transportista = t9.IdTransportista
-    INNER JOIN t_cliente as t10 ON t1.Cliente = t10.IdCliente
-    INNER JOIN t_ubicacion as t11 on t1.Origen = t11.IdUbicacion
-    INNER JOIN t_ubicacion as t12 on t1.Destino = t12.IdUbicacion
-    WHERE t1.Estatus = 0
-    GROUP BY t1.IdBoletas, t1.Placas, t1.Chofer, t10.Cliente, 
-            t7.TipoTransporte, t9.Transportista, t11.Ubicacion, t12.Ubicacion";
+       t1.Placas,
+       t1.Chofer,
+       t10.Cliente,
+       t7.TipoTransporte,
+       YEAR(t1.FechaCita) as año,
+       (SELECT STRING_AGG(Descripcion, ', ') WITHIN GROUP (ORDER BY Descripcion ASC)
+        FROM (SELECT DISTINCT t8.Descripcion
+              FROM t_boleta_det t2b
+              LEFT JOIN t_producto t8 ON t2b.Producto = t8.IdProducto
+              WHERE t2b.IdBoletasEnc = t1.Idboletas) AS productos) as Descripcion,
+       t9.Transportista,
+       t11.Ubicacion as Origen,
+       t12.Ubicacion as Destino,
+       (SELECT STRING_AGG(Placas, ', ') WITHIN GROUP (ORDER BY Placas ASC)
+        FROM (SELECT DISTINCT t2c.Placas
+              FROM t_boleta_det t2c
+              WHERE t2c.IdBoletasEnc = t1.Idboletas) AS placas) as PlacasRemolque
+FROM t_boleta_enc as t1 
+LEFT JOIN t_tipoTransporte as t7 ON t1.TipoTransporte = t7.IdTipoTransporte
+LEFT JOIN t_transportista as t9 on t1.Transportista = t9.IdTransportista
+INNER JOIN t_cliente as t10 ON t1.Cliente = t10.IdCliente
+INNER JOIN t_ubicacion as t11 on t1.Origen = t11.IdUbicacion
+INNER JOIN t_ubicacion as t12 on t1.Destino = t12.IdUbicacion
+WHERE t1.Estatus = 0  AND t1.FechaCita >= CAST(GETDATE() AS DATE) 
+  AND t1.FechaCita < DATEADD(DAY, 1, CAST(GETDATE() AS DATE))  
+GROUP BY t1.IdBoletas, t1.Placas, t1.Chofer, t10.Cliente, 
+         t7.TipoTransporte, t9.Transportista, t11.Ubicacion, t12.Ubicacion,t1.FechaCita";
     
     $sentencia = $Conexion->prepare($sql);
     $sentencia->execute();
