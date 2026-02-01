213f150dcb1e8a87d427d2ccaacde27901f885cb
AuxiliarSistemas3 <auxiliarsistemas3@alpasa.com.mx>
2026-02-01 13:42:05 -0600

    Update verificarMovimientoPendiente.php

--- Movil/verificarMovimientoPendiente.php
+++ Movil/verificarMovimientoPendiente.php
@@ -59,7 +59,7 @@ try {
 
     $movimiento = $stmtMovimiento->fetch(PDO::FETCH_ASSOC);
 
-    if ($stmtMovimiento->rowCount() > 0) {
+    if ($movimiento) {  
         $sqlEntradaInfo = "SELECT 
                                 IdPer,
                                 Observaciones,
